5f422ef82b192049bbd0ba8869fe7b62b70957a0
Max Lohmann <maxlohmann@MacBook-Pro-von-Max.fritz.box>
2025-07-13 05:05:23 +0200

    ✨ Add verified profile insert after login

--- app/support/page.tsx
+++ app/support/page.tsx
@@ -1,5 +1,6 @@
 'use client'
 
+export const dynamic = 'force-dynamic'
 import { useEffect, useState, ChangeEvent, FormEvent } from 'react'
 import { useSearchParams } from 'next/navigation'
 import { supabase } from '@/utils/supabaseClient'
