1667211dd3e2673267736fa6bae6f4f4aaf930b6
Max Lohmann <maxlohmann@MBP-von-Max.fritz.box>
2025-08-05 00:54:39 +0200

    Force rebuild for partner-dashboard

--- app/components/DealCard.tsx
+++ app/components/DealCard.tsx
@@ -1,5 +1,3 @@
-// components/DealCard.tsx
-
 type DealCardProps = {
   name: string
   description: string
@@ -35,14 +33,14 @@ export default function DealCard({
 
       {/* Infobereich */}
       <div className="flex flex-col items-center gap-5 text-center">
-        <h3 className="text-lg font-semibold">{name}</h3>
+        <h3 className="text-lg font-semibold text-[#003b5b]">{name}</h3>
         <p className="text-sm text-gray-600">{description}</p>
         <div className="bg-[#ca4b24] text-white px-8 py-3 rounded-lg text-xl font-bold min-w-[160px]">
           {reward} €
         </div>
         <button
           onClick={onClick}
-          className="bg-[#ca4b24] hover:bg-[#a33d1e] text-white px-8 py-3 rounded-lg text-lg font-medium min-w-[160px] transition"
+          className="cursor-pointer bg-[#ca4b24] hover:bg-[#a33d1e] text-white px-8 py-3 rounded-lg text-lg font-medium min-w-[160px] transition"
         >
           Jetzt sichern!
         </button>
